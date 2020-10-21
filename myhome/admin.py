1be28b68b8cae052cd80c81e4e6f60799a7e0813
Fabrizio <fabrizio.carlassara@gmail.com>
2020-10-21 20:56:18 -0300

    RoomType, Room, DeviceType, Device and User modes created

--- myhome/admin.py
+++ myhome/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import User, DeviceType, Device, RoomType, Room
+
+class DeviceAdmin(admin.ModelAdmin):
+    fields = ('type','status','room')
+
+class RoomAdmin(admin.ModelAdmin):
+    fields = ('type',)
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(DeviceType)
+admin.site.register(Device, DeviceAdmin)
+admin.site.register(RoomType)
+admin.site.register(Room, RoomAdmin)
--- myhome/models.py
+++ myhome/models.py
@@ -4,3 +4,45 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class RoomType(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Room(models.Model):
+    type = models.ForeignKey(RoomType, on_delete=models.CASCADE)
+
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'type' : self.type.name,
+            'devices' : [device.serialize() for device in self.devices.all()]
+        }
+
+    def __str__(self):
+        return f'{self.type.name}'
+
+class DeviceType(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+
+class Device(models.Model):
+    type = models.ForeignKey(DeviceType, on_delete=models.CASCADE)
+    status = models.CharField(max_length=10, default='off')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='devices')
+
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'type' : self.type.name,
+            'status' : self.status,
+            'room' : self.room
+        }
+    
+    def __str__(self):
+        return f'{self.type.name} ({self.id})'
+
